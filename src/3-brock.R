7abd488f28e7513198b4cb4410c5e0614b84f056
edisz <eduardszoecs@gmail.com>
2015-01-14 14:54:15 +0100

    rewritten example: no PB, no non-parametric stuff, one-sided Dunnett

--- src/3-brock.R
+++ src/3-brock.R
@@ -2,118 +2,39 @@
 ### Motivating Example - Count data
 ### Written by Eduard Szöcs
 
-### -------- Setup ------------------------------------------------------------
-prj <- "/home/edisz/Documents/Uni/Projects/PHD/6USETHEGLM/"
-datadir <- file.path(prj, "data")   # data
 
-### -------- Packages
+rm(list = ls()[!ls() %in% 'prj'])
+#####--------------------------------------------------------------------------
+### Setup project structure
+
+# Project Path
+## You have to change this!
+# prj <- "/home/edisz/Documents/Uni/Projects/PHD/6USETHEGLM/"
+# prj <- 'C:\\Users\\Edi\\Documents\\usetheglm'
+if(!exists("prj")){
+  stop("You need to create a object 'prj' that points to the top folder, 
+       e.g. prj <- '/home/edisz/Documents/Uni/Projects/PHD/4BFG/Project'!")
+}
+
+
+# Subfolder paths
+srcdir <- file.path(prj, "src")     # source code
+datadir <- file.path(prj, "data")   # data
+figdir <- file.path(prj, "report", "fig") # figures for latex
+
+
+#####--------------------------------------------------------------------------
+### install missing if needed!
 require(MASS)
 require(ggplot2)
-require(scales)
-require(reshape2)
+require(grid)
 require(plyr)
-require(lmtest)
 require(multcomp)
 
-### --------- Custom Functions
-#' pairwise wilcox.test
-#' @param y numeric; vector of data values
-#' @param g factor; grouping vector
-#' @param dunnett logical; if TRUE dunnett contrast, otherwise Tukey-contrasts
-#' @param padj character; method for p-adjustment, see ?p.adjust.
-pairwise_wilcox <- function(y, g, dunnett = TRUE, padj = 'holm'){
-  tc <- t(combn(nlevels(g), 2))
-  if(dunnett){
-    tc <- tc[tc[ ,1] == 1, ]
-  }
-  pval <- numeric(nrow(tc))
-  for(i in seq_len(nrow(tc))){
-    pval[i] <- wilcox.test(y[as.numeric(g) == tc[i, 1]], 
-                           y[as.numeric(g) == tc[i, 2]])$p.value
-  }
-  pval <- p.adjust(pval, padj)
-  names(pval) = paste(tc[,1], tc[,2], sep = '-')
-  return(pval)
-}
 
-#' Parametric bootstrap (PB)
-#' @param m1 full model
-#' @param m0 reduced model
-#' @param data data object used to fit the models
-#' @param npb number of boostraps
-
-myPBmodcomp <- function(m1, m0, data, npb){
-  ## create reference distribution of LR and coefs
-  myPBrefdist <- function(m1, m0, data){
-    # simulate from null
-    x0 <- simulate(m0)
-    # refit
-    newdata0 <- data
-    newdata0[ , as.character(formula(m0)[[2]])] <- x0
-    m1r <-  try(update(m1, .~., data = newdata0))
-    m0r <- try(update(m0, .~., data = newdata0))
-    # simulate from model
-    x1 <- simulate(m1)
-    # refit
-    newdata1 <- data
-    newdata1[ , as.character(formula(m1)[[2]])] <- x1
-    m1r1 <-  try(update(m1, .~., data = newdata1))
-    
-    # check convergence (otherwise return NA for LR)
-    if(!is.null(m0r[['th.warn']]) | !is.null(m1r[['th.warn']]) | 
-         inherits(m0r, "try-error") | inherits(m1r, "try-error")){
-      LR <- 'convergence error'
-    } else {
-      LR <- -2 * (logLik(m0r) - logLik(m1r))
-    }
-    # convergence for coefs
-    if(!is.null(m1r1[['th.warn']]) | inherits(m1r1, "try-error")){
-      coefs <- 'convergence error'
-    } else {
-      coefs <- coef(m1r1)
-    }
-    # return LR and coefs.
-    out <- list(LR = LR, coefs = coefs)
-    return(out)
-  }
-  
-  ## calculate reference distribution
-  ref <- replicate(npb, myPBrefdist(m1 = m1, m0 = m0, data = data), 
-                   simplify = FALSE)
-  LR <- sapply(ref, function(x) x[['LR']])
-  coefs <- lapply(ref, function(x) x[['coefs']])
-  
-  # check convergence
-  nconv_LR <-  LR == 'convergence error'
-  # rm those
-  LR <- as.numeric(LR[!nconv_LR])
-  nconv_LR <- sum(!nconv_LR)
-  
-  nconv_coefs <- sapply(coefs, function(x) any(x == 'convergence error'))
-  # rm those
-  coefs[nconv_coefs] <- NULL
-  nconv_coefs <- sum(!nconv_coefs)
-  coefs <- do.call(rbind, coefs)
-  
-  ## original stats
-  LRo <- c(-2 * (logLik(m0) - logLik(m1)))
-  COEFo <- coef(m1)
-  
-  ## LR p-value 
-  p.pb <- mean(c(LR, LRo) >= LRo, na.rm = TRUE)
-  
-  ## p-value for coef
-  # proportions of boostrap coefs > or < 0
-  twosidep<-function(coef){
-    p1 <- sum(coef > 0) / length(coef)
-    p2 <- sum(coef < 0) / length(coef)
-    p <- min(p1,p2) * 2
-    return(p)
-  }
-  p.coef <- apply(coefs, 2, twosidep)
-  return(list(nconv_LR = nconv_LR, nconv_coefs = nconv_coefs,
-              p.pb = p.pb, p.coef = p.coef))
-}
+#####--------------------------------------------------------------------------
+### Source defined functions
+source(file.path(srcdir, "functions.R"))
 
 
 
@@ -143,20 +64,10 @@ modlm <- lm(log_Abundance ~ Concentration, data = df)
 ## Inference, global test
 # F-test
 drop1(modlm, test = 'F')
-# LR-test
-drop1(modlm, test = 'Chisq')
 
-## Inference, parameters
-summary(modlm)
-coef(summary(modlm))
-# Wald-t
-prettyNum(p.adjust(coef(summary(modlm))[ , 'Pr(>|t|)'], method = 'holm'))[2:6]
-# same as
-summary(glht(modlm, linfct = mcp(Concentration = 'Dunnett')),  
-        test = adjusted('holm'))
-summary(glht(modlm, linfct = mcp(Concentration = 'Williams')),  
-        test = adjusted('holm'))
-
+## Inference, LOEC
+# one-sided Dunnett test
+summary(glht(modlm, linfct = mcp(Concentration = 'Dunnett'),  alternative = 'less'))
 
 ## predicted mean values with back transformation
 modlm_p <- predict(modlm, newdata = data.frame(Concentration = unique(df$Concentration)), 
@@ -172,7 +83,7 @@ modlm_upr <- (exp(modlm_p$fit + 1.96*modlm_p$se.fit) - 1) / A
 ### --------
 ### Poisson GLM
 modpois <- glm(Abundance ~ Concentration, data = df, family = poisson)
-summary(modpois)
+
 # residual deviance >> residual degrees of freedom
 modpois$deviance / modpois$df.residual
 # overdispersion!
@@ -180,19 +91,10 @@ modpois$deviance / modpois$df.residual
 ## Inference, global test
 # LR test
 drop1(modpois, test = 'Chisq')
-# # parametric bootstrap
-# modpois0 <- glm(Abundance ~ 1, data = df, family = poisson)
-# myPBmodcomp(modpois, modpois0, data = df, npb = 500)
-# # p-value of boostraped LR is 0.001996008
 
-## Inference, test of parameters
-# Wald-z
-summary(modpois)
-prettyNum(p.adjust(coef(summary(modpois))[ , 'Pr(>|z|)'], method = 'holm'))[2:6]
-summary(glht(modpois, linfct = mcp(Concentration = 'Dunnett')),  
-        test = adjusted('holm'))
-# parametric bootstrap
-# see above, all p = 0
+## Inference, LOEC
+## one-sided Dunnett test
+summary(glht(modpois, linfct = mcp(Concentration = 'Dunnett'), alternative = 'less'))
 
 ## predicted value on response scale
 modpois_p <- predict(modpois, newdata = data.frame(Concentration = unique(df$Concentration)), 
@@ -200,7 +102,6 @@ modpois_p <- predict(modpois, newdata = data.frame(Concentration = unique(df$Con
 modpois_fit <- modpois$family$linkinv(modpois_p$fit)
 modpois_lwr <- modpois$family$linkinv(modpois_p$fit - 1.96 * modpois_p$se.fit)
 modpois_upr <- modpois$family$linkinv(modpois_p$fit + 1.96 * modpois_p$se.fit)
-
 
 
 ### --------
@@ -211,13 +112,9 @@ modqpois <- glm(Abundance ~ Concentration, data = df, family = quasipoisson)
 # F test
 drop1(modqpois, test = 'F')
 
-## Inference, test of parameters
-# Wald-t
-summary(modqpois)
-prettyNum(p.adjust(coef(summary(modqpois))[ , 'Pr(>|t|)'], method = 'holm'))[2:6]
-# multicomp performs a Wald-z
-summary(glht(modqpois, linfct = mcp(Concentration = 'Dunnett')),  
-        test = adjusted('holm'))
+## Inference, LOEC
+# # one-sided Dunnett test
+summary(glht(modlm, linfct = mcp(Concentration = 'Dunnett'),  alternative = 'less'))
 
 ## predicted value on response scale
 modqpois_p <- predict(modqpois, newdata = data.frame(Concentration = unique(df$Concentration)), 
@@ -237,23 +134,11 @@ modnb <- glm.nb(Abundance ~ Concentration, data = df)
 # refit null model, to estimate theta of null
 modnb0 <- glm.nb(Abundance ~ 1, data = df)
 anova(modnb, modnb0, test = 'Chisq')
-# parametric bootstrap
-set.seed(1234)
-myPBmodcomp(modnb, modnb0, data = df, npb = 2000)
-# p-value of boostraped LR is 0.054
-# 1 model did not converge
 
-## Inference, test of parameters
-# Wald-z
-summary(modnb)
-prettyNum(p.adjust(coef(summary(modnb))[ , 'Pr(>|z|)'], method = 'holm'))[2:6]
-summary(glht(modnb, linfct = mcp(Concentration = 'Dunnett')),  
-        test = adjusted('holm'))
-# parametric bootstrap
-# see above, 
-# $p.coef
-# (Intercept) Concentration0.1 Concentration0.3   Concentration1   Concentration3  Concentration10 
-# 0.000            0.214            0.026            0.034            0.000            0.004 
+
+## Inference, LOEC
+# # one-sided Dunnett test
+summary(glht(modnb, linfct = mcp(Concentration = 'Dunnett'),  alternative = 'less'))
 
 ## predicted value on response scale
 modnb_p <- predict(modnb, newdata = data.frame(Concentration = unique(df$Concentration)), 
@@ -262,15 +147,6 @@ modnb_fit <- modnb$family$linkinv(modnb_p$fit)
 modnb_lwr <- modnb$family$linkinv(modnb_p$fit - 1.96 * modnb_p$se.fit)
 modnb_upr <- modnb$family$linkinv(modnb_p$fit + 1.96 * modnb_p$se.fit)
 
-
-
-### --------
-### Non-parametric
-
-# Kruskal-Wallis test
-kruskal.test(Abundance ~ Concentration, data = df)
-# pairwise Wilcox
-pairwise_wilcox(df$Abundance, factor(df$Concentration), dunnett = TRUE, padj = 'holm')
 
 
 ### --------------- Summarize --------------------------------------------------
@@ -319,6 +195,8 @@ p <- ggplot() +
   geom_path(aes(x = c(5-0.15, 5+0.55), y = c(220,220)), linetype = 'solid', size = 1) + 
   # pois
   geom_path(aes(x = c(2-0.15, 6+0.55), y = c(220-5,220-5)), linetype = '1111', size = 1) + 
+  # qpois 
+  geom_path(aes(x = c(5-0.15, 5+0.55), y = c(220-2*5,220-2*5)), linetype = 'dashed', size = 1) +
   # neg bin
   geom_path(aes(x = c(3-0.15, 3+0.55), y = c(220-3*5,220-3*5)), linetype = 'dotdash', size = 1) + 
   geom_path(aes(x = c(5-0.15, 6+0.55), y = c(220-3*5,220-3*5)), linetype = 'dotdash', size = 1) + 
@@ -336,5 +214,4 @@ p <- ggplot() +
         )
 p
 ggsave(file.path(figdir, 'example.pdf'), p, height = 6, width = 8)
-# ggsave(file.path(markdir, 'figure1.eps'), p)
 
